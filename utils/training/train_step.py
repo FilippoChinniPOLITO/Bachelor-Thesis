a3d1a6019378449eb1d81bc711376b63dcd27c12
filippochinni <filippochinni02@gmail.com>
2024-05-08 01:10:31 +0200

    Fix Weedmapping Training

--- utils/training/train_step.py
+++ utils/training/train_step.py
@@ -28,3 +28,31 @@ def train_step(dataloader, model, loss_fn, optimizer):
         #     loss, current = loss.item(), batch * len(X)
         #     print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
+
+def train_step_weedmapping(dataloader, model, loss_fn, optimizer):
+    # Set the Device
+    device = get_device()
+
+    # size = len(dataloader.dataset)
+
+    # Set the Model to Training Mode
+    model.train()
+
+    for batch, (X, y, _) in enumerate(dataloader):
+        # Mount data to device
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        # (Optional) Print Loss
+        # if batch % 100 == 0:
+        #     loss, current = loss.item(), batch * len(X)
+        #     print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
