21a5dfad2a44c345b3cd280d00f6fde5447ab4c5
filippochinni <filippochinni02@gmail.com>
2024-05-11 17:02:46 +0200

    Fix PSOSampler

--- utils/optimization/optuna_study_creator.py
+++ utils/optimization/optuna_study_creator.py
@@ -8,14 +8,14 @@ class OptunaStudyCreator:
         self.path_db = path_db
         self.session_num = session_num
 
-    def __call__(self, study_name, direction, sampler, pruner=None):
+    def __call__(self, study_name, storage, direction, sampler, pruner=None):
         folder_exists_check(self.path_db, self.session_num, f'storage_{study_name}')
         db_file = file_name_builder(self.path_db, self.session_num, f'storage_{study_name}', 'db')
 
-        storage = f'sqlite:///{db_file}'
+        storage_url = f'sqlite:///{db_file}' if storage else None
 
         return optuna.create_study(study_name=study_name,
-                                   storage=storage,
+                                   storage=storage_url,
                                    direction=direction,
                                    sampler=sampler,
                                    pruner=pruner)
