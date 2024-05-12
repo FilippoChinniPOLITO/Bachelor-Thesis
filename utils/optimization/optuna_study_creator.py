6489a7a5469ac78aee1ea2a488bea7cc001deaeb
filippochinni <filippochinni02@gmail.com>
2024-05-12 14:44:07 +0200

    Partial Fix Storage

--- utils/optimization/optuna_study_creator.py
+++ utils/optimization/optuna_study_creator.py
@@ -5,26 +5,29 @@ from utils.persistency.file_name_builder import folder_exists_check, file_name_b
 
 
 class OptunaStudyCreator:
-    def __init__(self, path_db, session_num):
+    def __init__(self, experiment_name, path_db, session_num):
+        self.experiment_name = experiment_name
         self.path_db = path_db
         self.session_num = session_num
 
     def __call__(self, study_name, storage, direction, sampler, pruner=None):
-        folder_exists_check(self.path_db, self.session_num, f'storage_{study_name}')
-        db_file = file_name_builder(self.path_db, self.session_num, f'storage_{study_name}', 'db')
+        folder_exists_check(self.path_db, '', f'{self.experiment_name}')
+        db_file = file_name_builder(self.path_db, '', f'{self.experiment_name}', 'db')
+
+        db_study_name = f'study_{study_name}_{self.session_num}'
 
         if storage:
             storage_url = f'sqlite:///{db_file}'
             storage_obj = RDBStorage(url=storage_url, engine_kwargs={"connect_args": {"timeout": 10}})
             try:
-                storage_obj.get_study_id_from_name(study_name)
-                raise ValueError(f"Study {study_name} already exists in the database.")
+                storage_obj.get_study_id_from_name(db_study_name)
+                raise ValueError(f"Study {db_study_name} already exists in the database.")
             except KeyError:
                 pass
         else:
             storage_obj = None
 
-        return optuna.create_study(study_name=study_name,
+        return optuna.create_study(study_name=db_study_name,
                                    storage=storage_obj,
                                    direction=direction,
                                    sampler=sampler,
