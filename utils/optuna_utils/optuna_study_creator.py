c53c12bbbf59bb655acbf44705b5b96e22489e85
filippochinni <filippochinni02@gmail.com>
2024-05-15 04:17:07 +0200

    Implement Study Deleting

--- utils/optuna_utils/optuna_study_creator.py
+++ utils/optuna_utils/optuna_study_creator.py
@@ -1,5 +1,6 @@
 import optuna
 from optuna.storages import RDBStorage
+from sqlalchemy import create_engine, text
 
 from utils.persistency.file_name_builder import folder_exists_check, file_name_builder, file_path_builder
 
@@ -57,3 +58,21 @@ class OptunaStudyCreator:
                           to_study_name=db_study_name,
                           from_storage=f'sqlite:///{db_file}',
                           to_storage=self.storage_obj)
+
+    def delete_study_from_db(self, study_name, session):
+        db_study_name = f'study_{study_name}_{session}'
+        db_file = file_path_builder(self.path_db, f'{self.experiment_name}', '', 'db')
+        storage_url = f'sqlite:///{db_file}'
+
+        engine = create_engine(storage_url)
+        with engine.connect() as connection:
+            # Delete from studies table
+            connection.execute(text(f"DELETE FROM studies WHERE study_name = :sname"), {"sname": db_study_name})
+            # Delete from trial_params table
+            connection.execute(text(f"DELETE FROM trial_params WHERE trial_id IN (SELECT trial_id FROM trials WHERE study_id NOT IN (SELECT study_id FROM studies))"))
+            # Delete from trial_values table
+            connection.execute(text(f"DELETE FROM trial_values WHERE trial_id IN (SELECT trial_id FROM trials WHERE study_id NOT IN (SELECT study_id FROM studies))"))
+            # Delete from trial_intermediate_values table
+            connection.execute(text(f"DELETE FROM trial_intermediate_values WHERE trial_id IN (SELECT trial_id FROM trials WHERE study_id NOT IN (SELECT study_id FROM studies))"))
+            # Delete from trials table
+            connection.execute(text(f"DELETE FROM trials WHERE study_id NOT IN (SELECT study_id FROM studies)"))
